ac3278f547ea6acbc609da61669d11ddc57d1a66
rajbalaraj <rajbalasr15@gmail.com>
2022-08-24 04:15:57 +0600

    index commit

---
+++ script/index.js
@@ -0,0 +1,29 @@
+buttons = document.getElementsByClassName('btn');
+for (let btn of buttons) {
+
+    btn.addEventListener('click', function (event) {
+        let parents = event.target.parentNode;
+        let titles = parents.getElementsByClassName('card-title');
+        for (let name of titles) {
+            addToList(name.innerText, btn);
+        }
+    })
+
+}
+let expense = document.getElementById('input-player-expense');
+let manager = document.getElementById('manager-expense');
+let coach = document.getElementById('coach-expense');
+
+document.getElementById('calculate-player-btn').addEventListener('click', function () {
+
+    if (getListLength() == 0) {
+        alert("Please Select some Player first");
+
+    } else if (checkInputError(expense) == true) {
+        expense.value = '';
+    } else {
+        let listLength = getListLength();
+        calculatePlayerExpense(listLength);
+    }
+})
+
